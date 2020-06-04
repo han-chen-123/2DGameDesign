c07140821fb656d3b325ca3afd5793ba5217d407
han-chen-123 <downeycwtt@gmail.com>
2020-06-04 13:45:42 +0800

    level1 level2 added

---
+++ Assets/Scripts/levelSelection/level2Select.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class level2Select : MonoBehaviour
+{
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            Invoke("SwitchLevel", 3f);
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D collision)
+    {
+        if (collision.tag == "Player")
+        {
+            CancelInvoke();
+        }
+    }
+
+    void SwitchLevel()
+    {
+        SceneManager.LoadScene("level2");
+    }
+}
